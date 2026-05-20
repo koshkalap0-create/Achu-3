96d5524e4fc33de31cda42907c4db0c951987249
Pooja Muraleedharan <poojamuraleedharan0@gmail.com>
2026-05-20 22:32:58 +0530

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Achu-3/',
+})
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
